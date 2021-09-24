1c4c4f2d0fbb701a54ff8f46c777586f9b5586b0
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2021-09-24 05:52:51 -0400

    -Add Ex.1124

---
+++ volume-11/1124/1124.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    // freopen("..\\uva\\p1124.in", "r", stdin);
+
+    /*ios_base::sync_with_stdio(0);
+      cin.tie(0);*/
+
+    ios::sync_with_stdio(false);
+
+    char cadena;
+    while (scanf("%c", &cadena) != EOF) {
+        printf("%c", cadena);
+    }
+
+    return 0;
+}
