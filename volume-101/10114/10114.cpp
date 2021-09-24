85942b3ce69cdd74a5df9f435b3edcaa1f6a2e09
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2021-09-24 05:56:13 -0400

    -Add Volume 101 Ex.10114

---
+++ volume-101/10114/10114.cpp
@@ -0,0 +1,46 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+    // freopen("..\\uva\\p_easy\\p10114\\p10114.in", "r", stdin);
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int loanDur, nRec;
+    double downPayment, loan, decP[101];
+
+    while (true) {
+        cin >> loanDur >> downPayment >> loan >> nRec;
+        if (loanDur < 0)
+            break;
+
+        int m;
+        double p;
+        while (nRec--) {
+            cin >> m >> p;
+            for (int i = m; i < 101; i++)
+                decP[i] = p;
+        }
+
+        int now = 0;
+        double monthlyPayment = loan / loanDur;
+        double curVal = (loan + downPayment) * (1 - decP[0]);
+        double curLoan = loan;
+        while (curVal < curLoan) {
+            now++;
+            curLoan -= monthlyPayment;
+            curVal = curVal * (1 - decP[now]);
+        }
+
+        cout << now << " month";
+        if (now != 1)
+            cout << "s";
+        cout << endl;
+    }
+
+    return 0;
+}
